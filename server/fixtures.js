69d8580e33091817457bd693eed38aacb9e72dd8
Eldar Babayev <eldarbabayev@live.com>
2016-03-27 21:47:19 +0500

    added routes for posts lists

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,22 +1,29 @@
 // Fixture data
 if (Posts.find().count() === 0) {
+
     var now = new Date().getTime();
     // create two users
     var tomId = Meteor.users.insert({
 	profile: { name: 'Tom Coleman' }
     });
+
     var tom = Meteor.users.findOne(tomId);
+
     var sachaId = Meteor.users.insert({
 	profile: { name: 'Sacha Greif' }
     });
+
     var sacha = Meteor.users.findOne(sachaId);
+
     var telescopeId = Posts.insert({
 	title: 'Introducing Telescope',
 	userId: sacha._id,
 	author: sacha.profile.name,
 	url: 'http://sachagreif.com/introducing-telescope/',
 	submitted: new Date(now - 7 * 3600 * 1000),
-	commentsCount: 2
+	commentsCount: 2,
+	upvoters: [],
+	votes: 0
     });
 
     Comments.insert({
@@ -31,7 +38,9 @@ if (Posts.find().count() === 0) {
 	postId: telescopeId,
 	userId: sacha._id,
 	author: sacha.profile.name,
-	submitted: new Date(now - 3 * 3600 * 1000), body: 'You sure can Tom!'
+	submitted: new Date(now - 3 * 3600 * 1000),
+	body: 'You sure can Tom!'
+	
     });
 
     Posts.insert({
@@ -40,7 +49,9 @@ if (Posts.find().count() === 0) {
 	author: tom.profile.name,
 	url: 'http://meteor.com',
 	submitted: new Date(now - 10 * 3600 * 1000),
-	commentsCount: 0
+	commentsCount: 0,
+	upvoters: [],
+	votes: 0
     });
 
     Posts.insert({
@@ -49,7 +60,9 @@ if (Posts.find().count() === 0) {
 	author: tom.profile.name,
 	url: 'http://themeteorbook.com',
 	submitted: new Date(now - 12 * 3600 * 1000),
-	commentsCount: 0
+	commentsCount: 0,
+	upvoters: [],
+	votes: 0
     });
 
     for (var i = 0; i < 10; i++) {
@@ -59,7 +72,9 @@ if (Posts.find().count() === 0) {
 	    userId: sacha._id,
 	    url: 'http://google.com/?q=test-' + i,
 	    submitted: new Date(now - i * 3600 * 1000),
-	    commentsCount: 0	    
+	    commentsCount: 0,
+	    upvoters: [],
+	    votes: 0
 	});
     }
 }
